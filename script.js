6c123c00b8050bc4a72e80b2d00671b9658929ba
Logan Copeland <logankcc@gmail.com>
2024-10-27 00:40:32 -0700

    Started development

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("playButton").addEventListener("click", function() {
+    // Navigate to the "Select Game Mode" page
+    window.location.href = "select-game-mode.html";
+});
+
+document.getElementById("singlePlayerButton").addEventListener("click", function() {
+    alert("Still in development!");
+});
+
+document.getElementById("multiPlayerButton").addEventListener("click", function() {
+    alert("Still in development!");
+});
